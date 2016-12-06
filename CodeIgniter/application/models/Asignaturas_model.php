ce1dbdfa9aae0d0768944edb177da36e10ed5fcc
Miguel González Aravena <miguel.gonzalez.93@gmail.com>
2016-12-06 02:22:15 -0300

    CRUD en CodeIgniter

---
+++ CodeIgniter/application/models/Asignaturas_model.php
@@ -0,0 +1,47 @@
+<?php
+if(!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Asignaturas_model extends CI_Model {
+  
+  public function __construct() {
+    parent::__construct();
+  }
+
+  public function set_asignatura($cod, $nom) {
+    $data = array(
+      'codigo' => $cod,
+      'nombre' => $nom
+    );
+
+    $this->db->insert('asignaturas', $data);
+  }
+
+  public function delete_asignatura($cod) {
+    $data = array(
+      'codigo' => $cod
+    );
+
+    $this->db->delete('asignaturas', $data);
+  }
+
+  public function update_asignatura($cod, $nom) {
+    $this->db->set('nombre', $nom);
+    $this->db->where('codigo', $cod);
+    $this->db->update('asignaturas');
+  }
+
+  public function get_asignaturas() {
+    $this->db->select('*');
+    $this->db->from('asignaturas');
+    $this->db->order_by('codigo DESC');
+    $query = $this->db->get();
+
+    if($query->num_rows() > 0) {
+      return $query;
+    } else {
+      return false;
+    }
+  }
+}
+
+?>
